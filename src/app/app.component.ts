26b83bcbea458dcd48429629206f689ae3cedfc6
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-07-02 11:24:46 +0700

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'M5-TH-AngularCalculator';
+  output: number;
+  firstInput: number;
+  secondInput: number;
+  operator: String;
+  constructor() {
+  }
+  // tslint:disable-next-line:typedef
+  onChangeFirst(value){
+    this.firstInput = Number(value);
+  }
+  // tslint:disable-next-line:typedef
+  onChangeSecond(value){
+    this.secondInput = Number(value);
+  }
+  // tslint:disable-next-line:typedef
+  onChangeSelect(value){
+    this.operator = value;
+  }
+  // tslint:disable-next-line:typedef
+  calculate(){
+    switch (this.operator) {
+      case "+":
+        this.output = this.firstInput + this.secondInput;
+        break;
+      case "-":
+        this.output = this.firstInput - this.secondInput;
+        break;
+      case "*":
+        this.output = this.firstInput * this.secondInput;
+        break;
+      case "/":
+        this.output = this.firstInput / this.secondInput;
+        break;
+      default:
+        break;
+    }
+  }
 }
